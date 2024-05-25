0ac4671cde1ae9f236443d77bbed3b4f1d888fc9
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2024-05-24 18:35:42 -0700

    Complete repeatString test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let newString = '';
+    for (i = 0; i < num; i++) {
+        newString += word;
+    }
+    return newString;
 };
 
 // Do not edit below this line
