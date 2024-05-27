0ed1e28fa4e0464a9419240741016ca64c938653
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2024-05-27 14:55:08 -0700

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    const sumArray = [];
+    // Identify whether arg1 and arg2 are positive whole integers
+    let smallArg;
+    let largeArg;
+    if (!Number.isInteger(arg1) || !Number.isInteger(arg2)) { 
+        return "ERROR";
+    } else if (arg1 > 0 && arg2 > 0) {
+        // Identify which arg is greater
+        if (arg1 > arg2) {
+            largeArg = arg1;
+            smallArg = arg2;
+        } else {
+            smallArg = arg1;
+            largeArg = arg2;
+        }
+    } else {
+        return "ERROR";
+    }
 
+    // Create an array with all values between arg1 and arg2, inclusive
+    for (let i = smallArg; i <= largeArg; i++) {
+        sumArray.push(i);
+    }
+
+    // Sum the values of the array and assign to sum
+    const sum = sumArray.reduce((partialSum, a) => partialSum + a, 0);
+    return sum;
 };
 
 // Do not edit below this line
