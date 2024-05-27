23f61400e3abd592f1f3649a8ca2a904b8811107
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2024-05-27 15:28:31 -0700

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  const tempCels = (tempFahr -32) * (5/9);
+  let roundedCels = Math.round(tempCels * 10) / 10
+  return roundedCels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+  const tempFahr = tempCels * (9/5) + 32;
+  let roundedFahr = Math.round(tempFahr * 10) / 10
+  return roundedFahr;
 };
 
 // Do not edit below this line
