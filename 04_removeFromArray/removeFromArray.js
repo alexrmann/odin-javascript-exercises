2d7f1bcbfbc3803b039c77ede5b2601db45e7f32
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2024-05-27 14:22:28 -0700

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  const newArray = [];
 
+  // For each item in the original array, perform a function that accepts the argument "item" (which represents the item at each index in the ...args rest parameter). If item is NOT included in the args rest parameter array, push it into newArray.
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
